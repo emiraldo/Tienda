aa71b0ede8f9acb7309d9eea317ed97e38810def
Emiraldo Lozano Sanchez <emiraldounillanos@gmail.com>
2015-02-28 20:59:51 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script type="text/javascript" src="js/script.js"></script>
+	</head>
+
+	<body>
+		<p>Hola mundo</p>
+	</body>
+</html>
