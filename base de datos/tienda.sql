2e4f7993078f312611697559f6ac6bda71d9adab
Emiraldo Lozano Sanchez <proyectTSP1@gmail.com>
2015-03-11 23:14:47 -0500

    solucion script sql

--- base de datos/tienda.sql
+++ base de datos/tienda.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-03-2015 a las 03:31:41
+-- Tiempo de generación: 12-03-2015 a las 05:13:45
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -42,6 +42,15 @@ CREATE TABLE IF NOT EXISTS `categoria` (
   `DESCRIPCION` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`ID`, `DESCRIPCION`) VALUES
+(1, 'Deportes'),
+(2, 'Accion'),
+(3, 'Aventura');
+
 -- --------------------------------------------------------
 
 --
@@ -56,6 +65,14 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `USER` varchar(15) NOT NULL,
   `PASS` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`CEDULA`, `NOMBRE`, `APELLIDO`, `TELEFONO`, `USER`, `PASS`) VALUES
+('11', '11', '11', '11', '11', '11'),
+('1121930903', 'Emiraldo', 'LOZANO', '6846220', 'emiraldo', '123');
 
 -- --------------------------------------------------------
 
@@ -73,11 +90,14 @@ CREATE TABLE IF NOT EXISTS `juego` (
   `IMAGEN` varchar(100) DEFAULT NULL,
   `VIDEO` varchar(20) NOT NULL,
   `IDCATEGORIA` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `juego`
+--
 
 INSERT INTO `juego` (`IDJUEGO`, `NOMBRE`, `DESCRIPCION`, `CANTIDAD`, `PRECIO`, `PLATAFORMA`, `IMAGEN`, `VIDEO`, `IDCATEGORIA`) VALUES
-(1, 'FIFA15', 'FIFA 15 recrea con todo detalle el f&uacute;tbol para que los aficionados sientan la emoci&oacute;n del deporte rey como nunca antes. S&eacute; testigo de c&oacute;mo el p&uacute;blico anima y corea desde las gradas a sus equipos tal y como pasa en la realidad, adem&aacute;s de escuchar a los comentaristas seguir la trama del juego con las Presentaciones Din&aacute;micas de Partido.', '93', '14500.00', 'XBOX/PS/PC', 'img/Fifa15.jpg', 'TnTYVT6lwBc', 1),
+(1, 'FIFA15', 'FIFA 15 recrea con todo detalle el f&uacute;tbol para que los aficionados sientan la emoci&oacute;n del deporte rey como nunca antes. S&eacute; testigo de c&oacute;mo el p&uacute;blico anima y corea desde las gradas a sus equipos tal y como pasa en la realidad, adem&aacute;s de escuchar a los comentaristas seguir la trama del juego con las Presentaciones Din&aacute;micas de Partido.', '92', '14500.00', 'XBOX/PS/PC', 'img/Fifa15.jpg', 'TnTYVT6lwBc', 1),
 (2, 'Grand Theft Auto V  ', 'Quinta entrega de la afamada saga de juegos sandbox de Rockstar Games. Con una atrevida nueva direcci&oacute;n en la libertad en mundo abierto, la narrativa, tres protagonistas (Michael, Trevor y Franklin), la jugabilidad basada en misiones y el multijugador online, Grand Theft Auto V se centra en la b&uacute;squeda del todopoderoso dinero en un reinventado Sur de California en la actualidad.', '35', '15000.00', 'XBOX/PS/PC', 'img/GTAV.jpg', 'QkkoHAzjnUs', 3),
 (3, 'PES 2015', 'Hace realidad el lema -el campo es nuestro-. Gracias a la combinaci&oacute;n de esfuerzos de los equipos de Producci&oacute;n de PES de Tokio y Windsor, han sido recreados perfectamente los momentos de total nerviosismo y emoci&oacute;n del f&uacute;tbol de primera categor&iacute;a.', '80', '12000.00', 'XBOX/PS/PC', 'img/PES2015.jpg', 'Tv13s6WFI-Y', 1),
 (4, 'FAR CRY 4', 'Desarrollado y distribuido por Ubisoft para PC, PlayStation 3, PlayStation 4, Xbox 360 y Xbox One, es la cuarta entrega de la saga de acci&oacute;n first person shooter Far Cry en la que viajaremos hasta el Himalaya para luchar contra un tirano amo y se&ntilde;or de sus tierras.', '12', '8000.00', 'XBOX/PS/PC', 'img/FC4.jpg', 'e9al_k8e93I', 2),
@@ -85,7 +105,6 @@ INSERT INTO `juego` (`IDJUEGO`, `NOMBRE`, `DESCRIPCION`, `CANTIDAD`, `PRECIO`, `
 (6, 'CALL OF DUTY: ADVANCED WARFARE ', 'Es una nueva entrega de la saga de disparos de Activision ambientado en un futuro 40 a&ntilde;os en el tiempo en el que los contratistas militares privados se han convertido en los grandes actores de la guerra internacional. El juego cuenta con Kevin Spacey como gran estrella, y nos ofrece armamento futurista, incluyendo exoesqueletos, armaduras y otros elementos de ciencia ficci&oacute;n.', '87', '15000.00', 'PC/XBOX/PS', 'img/COD.jpg', 'aoIiPTDz23Y', 2),
 (7, 'GEARS OF WAR 3', 'Es el final de la trilog&iacute;a conocida actualmente. Desarrollado por Epic Games en exclusiva para Xbox 360. El jugador se convierte en Marcus Fenix, h&eacute;roe de guerra y l&iacute;der del pelot&oacute;n Delta, quien debe salvar a la humanidad, ya que 18 meses despu&eacute;s de la ca&iacute;da del &uacute;ltimo basti&oacute;n humano, la guerra contra los Locust contin&uacute;a, mientras, bajo la superficie, una temible amenaza infecta el planeta.', '20', '11000.00', 'XBOX/PS', 'img/GOW3.jpg', 'n7Te5fcnrUA', 2),
 (8, 'CALL OF DUTY: GHOSTS', 'Desarrollado por Infinity Ward y distribuido por Activision. La acci&oacute;n transcurre  diez a&ntilde;os despu&eacute;s de un evento que devast&oacute; a las masas; por lo que USA ha dejado de ser una superpotencia. Una de las pocas fuerzas especiales de la naci&oacute;n, los Ghosts, tendr&aacute;n que combatir contra un poder global tecnol&oacute;gicamente superior reci&eacute;n surgido. Y no ser&aacute; por la libertad, sino simplemente por sobrevivir.', '9', '9000.00', 'XBOX/PS', 'img/CODG.jpg', 'Zxnx3W-HA18', 2);
-
 
 -- --------------------------------------------------------
 
@@ -100,7 +119,14 @@ CREATE TABLE IF NOT EXISTS `prestamo` (
   `PAGO` decimal(10,2) DEFAULT NULL,
   `IDCLIENTE` decimal(10,0) DEFAULT NULL,
   `IDJUEGO` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `prestamo`
+--
+
+INSERT INTO `prestamo` (`IDPRESTAMO`, `FECHA1`, `FECHA2`, `PAGO`, `IDCLIENTE`, `IDJUEGO`) VALUES
+(1, '2015-03-11', '2015-03-12', '14500.00', '1121930903', 1);
 
 --
 -- Índices para tablas volcadas
@@ -138,12 +164,12 @@ ALTER TABLE `prestamo`
 -- AUTO_INCREMENT de la tabla `juego`
 --
 ALTER TABLE `juego`
-MODIFY `IDJUEGO` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `IDJUEGO` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT de la tabla `prestamo`
 --
 ALTER TABLE `prestamo`
-MODIFY `IDPRESTAMO` int(10) NOT NULL AUTO_INCREMENT;
+MODIFY `IDPRESTAMO` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Restricciones para tablas volcadas
 --
